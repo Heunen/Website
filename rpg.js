d489f8c3114e40739ad71d825075bfd5a15df3c7
Heunen <68558886+Heunen@users.noreply.github.com>
2020-12-12 18:26:13 +0100

    Revert "Merge branch 'main' of https://github.com/Heunen/Website into main"
    This reverts commit 9d2287eeb2e41c4b8b6d3f8e5610cc4cce746299, reversing
    changes made to 889454e75f4c46342a0015db1950ab71c499067a.

--- rpg.js
+++ rpg.js
@@ -1,4 +1,3 @@
-
 "use strict"
 
 // initialisation du tableau des personnages Ã  vide
@@ -20,28 +19,4 @@ function ajouterPersonnage(formulaire) {
   // Ainsi on est sûr de ne pas envoyer le formulaire (et de ne pas rafraichir la page)
   document.getElementById("jeu").innerHTML = "Bienvenue à toi "+ personnage[0] + " la suite de ton aventure est en préparation par nos développeurs";
   return false;
-
-"use strict"
-
-// initialisation du tableau des personnages Ã  vide
-// un personnage est un tableau de cette forme [ nom, age, classe, sexe, niveau]
-var personnage = [];
-}
-
-
-function ajouterPersonnage(formulaire) {  
-  
-  // enregistrer le personnage dans le tableau
-  // [ nom, age, classe, sexe, niveau] 
-  personnage.push( formulaire.name.value, formulaire.age.value,
-                      formulaire.classe.value, formulaire.sexe.value, 1 );
-  
-  
-  // DÃ©terminer le titre et la race dans le message en fonction du sexe
-     
-  // Pour l'instant, on renvoie toujours false
-  // Ainsi on est sûr de ne pas envoyer le formulaire (et de ne pas rafraichir la page)
-  document.getElementById("jeu").innerHTML = "Bienvenue à toi "+ personnage[0] + " la suite de ton aventure est en préparation par nos développeurs";
-  return false;
-
 }
