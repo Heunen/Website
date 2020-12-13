aedbefde6e6a7e39770c188c844f683aa4a1baf5
Heunen <68558886+Heunen@users.noreply.github.com>
2020-12-13 01:44:18 +0100

    Ahout d'instance Intro

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Website
 Projet de groupe.
+
+
+
+Js:
+Fonction articleHtml :
+
+La fonction articleHtml permet de génèrer dans l'espace "section avec l'id jeu" l'interface de notre rpg.
+Elle supprime tout ce qui est dans cette section et la remplace par du nouveau code html basé en fonction de l'instance auquel on se situe
+dans le jeu.
+La fonction articleHtml crée deux article dont l'id correspond à l'instance auquel on se situe. Le premier artiche génèrer à en prefixe le mot "image"
+et le second le mot "Text". 
+Le premier article a pour but d'afficher une image. Tandis que le second à pour but de génerer le text + les interractions possibles.
+Les arguments de cette fonction sont : le text et le nom de l'instance
+
+Fonctionnement du code :
+Chaque instance aura sa fonction afin de pouvoir la modifier aissement. Une fonction d'instance appelera la fonction articleHtml afin de génèrer
+le code html.
--- rpg.js
+++ rpg.js
@@ -4,7 +4,8 @@
 // un personnage est un tableau de cette forme [ nom, age, classe, sexe, niveau]
 var personnage = [];
 
-
+//paramètre : les données du formulaire
+//rempli le tableau personnage de ses données.
 function ajouterPersonnage(formulaire) {  
   
   // enregistrer le personnage dans le tableau
@@ -12,11 +13,43 @@ function ajouterPersonnage(formulaire) {
   personnage.push( formulaire.name.value, formulaire.age.value,
                       formulaire.classe.value, formulaire.sexe.value, 1 );
   
+  //on lance la première instance : intro
+  instanceIntro();
   
-  // DÃ©terminer le titre et la race dans le message en fonction du sexe
-     
   // Pour l'instant, on renvoie toujours false
   // Ainsi on est sûr de ne pas envoyer le formulaire (et de ne pas rafraichir la page)
-  document.getElementById("jeu").innerHTML = "Bienvenue à toi "+ personnage[0] + " la suite de ton aventure est en préparation par nos développeurs";
   return false;
+}
+
+
+//Instance intro : gènere qu'un seul bouton commencer l'aventure qui lance l'instance : Aventure 
+function instanceIntro(){
+  let nomDeInstance="intro";	
+  let texte="Bienvenue à toi "+ personnage[0] +'!'; // première ligne
+  texte+="<br> Pour lancer l'aventure appuis sur le bouton 'lancer l'aventure. "
+  texte+="<br> <button onClick='instanceAventure()'>Lancer l'aventure</button>";
+  texte+="<button onClick='console.log(personnage)'>affficher le personnage dans la console</button>";
+  articleHtml(nomDeInstance,texte);
+  
+}
+
+//function test
+function instanceAventure(){     
+  let texte="Bienvenue à toi "+ personnage[0] +'!'; // première ligne
+  texte+="<br>AVENTURE "
+  texte+="<br> <button onClick='instanceAventure'>rien</button>";
+  articleHtml("intro",texte);
+  
+}
+
+
+//paramètre : le nom de l'instance, et le texte à afficher dans l'article text
+// modifie la section(id jeu) et y crée deux article tel que le premier article a comme id: image+nomDeInstance et le second texte+nomDeInstance
+// et le paramètre texte est injecter dans le deuxième article.
+function articleHtml(nomDeInstance,texte){
+	let mHtml= '<article id="'+nomDeInstance+'Image"></article>'
+			   + '<article id="'+nomDeInstance+'Text">'
+			   + texte
+               + "</article>";
+	document.getElementById("jeu").innerHTML = mHtml;
 }
