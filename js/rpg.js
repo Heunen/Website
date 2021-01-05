4a08930a5ec9a1de3014d4fe62c746aef60070d2
Heunen <68558886+Heunen@users.noreply.github.com>
2021-01-05 17:36:27 +0100

    Update rpg.js

--- js/rpg.js
+++ js/rpg.js
@@ -751,7 +751,7 @@ function combatBoss(avancementHist){
 				compteurEnnemis++;
 				texte = "Vous avez asséné un coup fatal à Volsung. Tout le monde se tait et un silence de plomb tombe sur le camp."+
 								"<br> Vous vous approchez de Volsung. Les brigands ont les yeux rivés sur vous."+
-								"<br><button onClick='combatBoss(3)'>Vérifier si Volsung est mort</button> <button onClick='combatBoss(102)'>Planter votre arme dans le corps de Volsung</button>";
+								"<br><button onClick='combatBoss(3)'>Vérifier si Volsung est mort> <button onClick='combatBoss(102)'>Planter votre arme dans le corps de Volsung</button>";
 			}
 			else{ texte = "<h1>Vous êtes mort.</h1>";}
 			break;
